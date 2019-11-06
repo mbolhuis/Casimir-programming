645f1102276e7192868137de190dcd48a441a8a1
Maarten Bolhuis <m.bolhuis@tudelft.nl>
2019-11-06 13:29:55 +0100

    Function added for calculating the circumference of a cricle

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
+import numpy as np
+
 print('Hello World!')
+
+
+
+#function for determining the circumference of  a circle
+def cir(r):
+	"""This function calculates the circumference of a circle
+	input r: radius of the circle
+	outpyt l: circumference of the circle"""
+ 
+	return 2*np.pi*r
+
+r = float(input("What is the radius of the circle[cm]?:"))
+
+print('The circumference of the circle is %0.2f cm'% cir(r)) 

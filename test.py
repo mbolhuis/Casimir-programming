9f6039e823daf836137f522a49023a8c225f50d7
Maarten Bolhuis <m.bolhuis@tudelft.nl>
2019-11-06 13:37:41 +0100

    Add: Function for calculating the area of a circle

--- test.py
+++ test.py
@@ -8,10 +8,20 @@ print('Hello World!')
 def cir(r):
 	"""This function calculates the circumference of a circle
 	input r: radius of the circle
-	outpyt l: circumference of the circle"""
+	output l: circumference of the circle"""
  
 	return 2*np.pi*r
+#function for determining the area of a circle
+def area(r):
+	"""This function calculates the area of a circle
+        input r: radius of the circle
+        output a: area of the circle"""
+	
+	return np.pi*r**2
+
+
 
 r = float(input("What is the radius of the circle[cm]?:"))
 
 print('The circumference of the circle is %0.2f cm'% cir(r)) 
+print('The ara of the circle is %0.2f cm^2' % area(r))

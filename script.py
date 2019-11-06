3a911bc317d81edcb6b83a8be78f5fb6400b5a97
Maarten Bolhuis <m.bolhuis@tudelft.nl>
2019-11-06 14:08:59 +0100

    import test.py in script.py

--- script.py
+++ script.py
@@ -1,1 +1,6 @@
+import test
 print('This is file is called script.py')
+
+r = float(input('What is the radius of the circle[cm]?:'))
+print('The circumference of the circle is %0.2f' % test.cir(r))
+print('The area  of the circle is %0.2f'% test.area(r))
